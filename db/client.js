5507a454ce2da6fe2827edaa109bb488410b0d21
MarcosPerez16 <marcos.perez06@yahoo.com>
2024-04-16 07:09:11 -0700

    installed express and connected backend server to PostgreSQL database

--- db/client.js
+++ db/client.js
@@ -1,13 +1,14 @@
 // Connect to DB
-const { Client } = require('pg')
+const { Client } = require("pg");
 
 // change the DB_NAME string to whatever your group decides on
-const DB_NAME = 'acc-capstone'
+const DB_NAME = "ecommerce_db";
 
-const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`
+const DB_URL =
+  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 
-const client = new Client(DB_URL)
+const client = new Client(DB_URL);
 
 // for deployment on Render.io you will need to create options in Client() for password, user, and host
 
-module.exports = client
+module.exports = client;
